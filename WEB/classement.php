6efe755cf30a9bc7a60a0bf7d8cc5d599d4e535b
ndanel <nath.dnl80@gmail.com>
2021-03-19 16:46:48 +0100

    Ebauche page classement

---
+++ WEB/classement.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require_once("classClassement.php");
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="classement.css">
+    <title>Classement</title>
+</head>
+<body>
+    <!--Haut de page-->
+    <div>
+    </div>
+    <!--Milieu de page-->
+    <div class="mid">
+        <table>
+            <tr>
+                <th>Rang</th>
+                <th>Nom</th>
+                <th>Temps</th>
+                <th>Classe</th>
+                <th>Tour</th>
+            </tr>
+            <?php
+            //Création du classement 
+            ?>
+        </table>
+    </div>
+    <!--Bas de page-->
+    <div>
+    </div>
+</body>
+</html>
