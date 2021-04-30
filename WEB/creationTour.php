9c3923a90aafa19344c7181add9c4d1989572f96
Colbert1 <grgeoire.colbert@outlook.fr>
2021-04-30 15:20:46 +0200

    menu deroulant select nom course et maj carnet de bord et seq

---
+++ WEB/creationTour.php
@@ -0,0 +1,14 @@
+<?php
+/*********************************
+Paramétrage du tour : Distance selon le nom de la course
+ ********************************/
+session_start();
+require_once("bdd.php");
+require("class/classTour.php");
+
+$req = $this->_bdd-> query("SELECT crs_nom FROM course_tbl");
+echo '<select name="nomCourse">';
+while ($donnees->fetch($req)) {
+    echo '<option value="' . $donnees['crs_nom'] . '"></option>';
+}
+echo '</select>';
