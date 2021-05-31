6e91d96e0db64896a17b10c6335f43dc9ce7c507
Colbert1 <gcolbert@la-providence.net>
2021-05-31 14:03:36 +0200

    export bdd

--- projetcross_lapro.sql
+++ projetcross_lapro.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 29, 2021 at 11:39 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.19
+-- Hôte : localhost:3306
+-- Généré le : lun. 31 mai 2021 à 14:03
+-- Version du serveur :  10.5.10-MariaDB-2
+-- Version de PHP : 7.4.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,18 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `projetcross_lapro`
+-- Base de données : `projetcross_lapro`
 --
-CREATE DATABASE IF NOT EXISTS `projetcross_lapro` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `projetcross_lapro` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `projetcross_lapro`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `classeparticipante_tbl`
+-- Structure de la table `classeparticipante_tbl`
 --
 
-DROP TABLE IF EXISTS `classeparticipante_tbl`;
 CREATE TABLE `classeparticipante_tbl` (
   `clp_id` int(11) NOT NULL,
   `cl_id` int(11) NOT NULL,
@@ -38,7 +36,15 @@ CREATE TABLE `classeparticipante_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `classeparticipante_tbl`
+-- RELATIONS POUR LA TABLE `classeparticipante_tbl`:
+--   `cl_id`
+--       `classe_tbl` -> `cl_id`
+--   `crs_id`
+--       `course_tbl` -> `crs_id`
+--
+
+--
+-- Déchargement des données de la table `classeparticipante_tbl`
 --
 
 INSERT INTO `classeparticipante_tbl` (`clp_id`, `cl_id`, `crs_id`) VALUES
@@ -50,17 +56,20 @@ INSERT INTO `classeparticipante_tbl` (`clp_id`, `cl_id`, `crs_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `classe_tbl`
+-- Structure de la table `classe_tbl`
 --
 
-DROP TABLE IF EXISTS `classe_tbl`;
 CREATE TABLE `classe_tbl` (
   `cl_id` int(11) NOT NULL,
   `cl_nom` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `classe_tbl`
+-- RELATIONS POUR LA TABLE `classe_tbl`:
+--
+
+--
+-- Déchargement des données de la table `classe_tbl`
 --
 
 INSERT INTO `classe_tbl` (`cl_id`, `cl_nom`) VALUES
@@ -72,10 +81,9 @@ INSERT INTO `classe_tbl` (`cl_id`, `cl_nom`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `course_tbl`
+-- Structure de la table `course_tbl`
 --
 
-DROP TABLE IF EXISTS `course_tbl`;
 CREATE TABLE `course_tbl` (
   `crs_id` int(11) NOT NULL,
   `crs_date` date NOT NULL,
@@ -83,7 +91,11 @@ CREATE TABLE `course_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `course_tbl`
+-- RELATIONS POUR LA TABLE `course_tbl`:
+--
+
+--
+-- Déchargement des données de la table `course_tbl`
 --
 
 INSERT INTO `course_tbl` (`crs_id`, `crs_date`, `crs_nom`) VALUES
@@ -94,17 +106,20 @@ INSERT INTO `course_tbl` (`crs_id`, `crs_date`, `crs_nom`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dossard_tbl`
+-- Structure de la table `dossard_tbl`
 --
 
-DROP TABLE IF EXISTS `dossard_tbl`;
 CREATE TABLE `dossard_tbl` (
   `ds_id` int(11) NOT NULL,
   `ds_num` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `dossard_tbl`
+-- RELATIONS POUR LA TABLE `dossard_tbl`:
+--
+
+--
+-- Déchargement des données de la table `dossard_tbl`
 --
 
 INSERT INTO `dossard_tbl` (`ds_id`, `ds_num`) VALUES
@@ -124,10 +139,9 @@ INSERT INTO `dossard_tbl` (`ds_id`, `ds_num`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `participant_tbl`
+-- Structure de la table `participant_tbl`
 --
 
-DROP TABLE IF EXISTS `participant_tbl`;
 CREATE TABLE `participant_tbl` (
   `pt_id` int(11) NOT NULL,
   `us_id` int(11) NOT NULL,
@@ -136,22 +150,42 @@ CREATE TABLE `participant_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `participant_tbl`
+-- RELATIONS POUR LA TABLE `participant_tbl`:
+--   `crs_id`
+--       `course_tbl` -> `crs_id`
+--   `ds_id`
+--       `dossard_tbl` -> `ds_id`
+--   `us_id`
+--       `user_tbl` -> `us_id`
+--
+
+--
+-- Déchargement des données de la table `participant_tbl`
 --
 
 INSERT INTO `participant_tbl` (`pt_id`, `us_id`, `crs_id`, `ds_id`) VALUES
 (1, 5, 1, 1),
 (2, 1, 1, 2),
 (3, 3, 2, 4),
-(4, 6, 1, 8);
+(4, 6, 1, 8),
+(6, 3, 1, NULL),
+(8, 11, 1, NULL),
+(10, 11, 1, NULL),
+(11, 11, 1, NULL),
+(13, 11, 1, NULL),
+(15, 3, 1, NULL),
+(17, 6, 2, 12),
+(18, 11, 1, NULL),
+(20, 3, 2, NULL),
+(21, 11, 4, NULL),
+(22, 11, 2, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `temps_tbl`
+-- Structure de la table `temps_tbl`
 --
 
-DROP TABLE IF EXISTS `temps_tbl`;
 CREATE TABLE `temps_tbl` (
   `ts_id` int(11) NOT NULL,
   `pt_id` int(11) NOT NULL,
@@ -160,22 +194,31 @@ CREATE TABLE `temps_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `temps_tbl`
+-- RELATIONS POUR LA TABLE `temps_tbl`:
+--   `tr_id`
+--       `tour_tbl` -> `tr_id`
+--   `pt_id`
+--       `participant_tbl` -> `pt_id`
+--
+
+--
+-- Déchargement des données de la table `temps_tbl`
 --
 
 INSERT INTO `temps_tbl` (`ts_id`, `pt_id`, `ts_temps`, `tr_id`) VALUES
 (3, 2, '00:16:48', 1),
 (4, 1, '00:40:13', 1),
 (5, 3, '00:03:13', 1),
-(6, 2, '00:15:00', 3);
+(6, 2, '00:15:00', 3),
+(7, 17, '07:54:26', 1),
+(8, 17, '04:55:11', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tour_tbl`
+-- Structure de la table `tour_tbl`
 --
 
-DROP TABLE IF EXISTS `tour_tbl`;
 CREATE TABLE `tour_tbl` (
   `tr_id` int(11) NOT NULL,
   `tr_distance` int(11) NOT NULL,
@@ -184,7 +227,13 @@ CREATE TABLE `tour_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `tour_tbl`
+-- RELATIONS POUR LA TABLE `tour_tbl`:
+--   `crs_id`
+--       `course_tbl` -> `crs_id`
+--
+
+--
+-- Déchargement des données de la table `tour_tbl`
 --
 
 INSERT INTO `tour_tbl` (`tr_id`, `tr_distance`, `tr_numero`, `crs_id`) VALUES
@@ -195,10 +244,9 @@ INSERT INTO `tour_tbl` (`tr_id`, `tr_distance`, `tr_numero`, `crs_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_tbl`
+-- Structure de la table `user_tbl`
 --
 
-DROP TABLE IF EXISTS `user_tbl`;
 CREATE TABLE `user_tbl` (
   `us_id` int(11) NOT NULL,
   `us_nom` varchar(30) NOT NULL,
@@ -210,7 +258,13 @@ CREATE TABLE `user_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user_tbl`
+-- RELATIONS POUR LA TABLE `user_tbl`:
+--   `cl_id`
+--       `classe_tbl` -> `cl_id`
+--
+
+--
+-- Déchargement des données de la table `user_tbl`
 --
 
 INSERT INTO `user_tbl` (`us_id`, `us_nom`, `us_prenom`, `us_mail`, `us_passwd`, `us_status`, `cl_id`) VALUES
@@ -219,16 +273,15 @@ INSERT INTO `user_tbl` (`us_id`, `us_nom`, `us_prenom`, `us_mail`, `us_passwd`, 
 (4, 'nathan', 'nathan', 'nathan@nathan.fr', '$argon2id$v=19$m=65536,t=4,p=1$MHdJbGZ0Y2VzVy5ZbWR0RQ$UbVl76wsFEAo5TfThOEilXetHUl4Rk57df2g0MroUVw', 0, 3),
 (5, 'colb', 'Laurent', 'gc@la-providence.net', '$argon2id$v=19$m=65536,t=4,p=1$SUNvVkhzS3ZXNXFRbDhTZA$qz887ay1mq7LSC9KAlaotmO+AT1QHKBVrEtD2vnXxRc', 0, 4),
 (6, 'Dupont', 'Pierre', 'ppp@la-providence.net', '$argon2id$v=19$m=65536,t=4,p=1$ZkxjRS8vbGJsNkkxZkw5bQ$mYHZ39x4qHS5EwSA5rktqZkmVt+kq62OltGQY0v5zuQ', 0, 4),
-(8, 'Colbert', 'Grégoire', 'gcolbert@la-providence.net', '123456', 0, 2),
-(9, 'test', 'test', 'test@test.fr', 'test', 0, 1),
-(10, 'test', 'test', 'test@test.test', '$2y$10$3DuORFPnsh5/UCGZ1nQz1uYsIgJD/Mv8JvEUJt0MzYJrlaqiVFULK', 0, 2);
+(10, 'test', 'test', 'test@test.test', '$2y$10$3DuORFPnsh5/UCGZ1nQz1uYsIgJD/Mv8JvEUJt0MzYJrlaqiVFULK', 0, 2),
+(11, 'Colbert', 'Grégoire', 'gcolbert@la-providence.net', '$2y$10$b.Ir2T/2SAMkeyDwyWVUbOWbhqfpbSBC/jjjPzhpXppauVmlRMmdu', 0, 2);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `classeparticipante_tbl`
+-- Index pour la table `classeparticipante_tbl`
 --
 ALTER TABLE `classeparticipante_tbl`
   ADD PRIMARY KEY (`clp_id`),
@@ -236,25 +289,25 @@ ALTER TABLE `classeparticipante_tbl`
   ADD KEY `crs_id` (`crs_id`);
 
 --
--- Indexes for table `classe_tbl`
+-- Index pour la table `classe_tbl`
 --
 ALTER TABLE `classe_tbl`
   ADD PRIMARY KEY (`cl_id`);
 
 --
--- Indexes for table `course_tbl`
+-- Index pour la table `course_tbl`
 --
 ALTER TABLE `course_tbl`
   ADD PRIMARY KEY (`crs_id`);
 
 --
--- Indexes for table `dossard_tbl`
+-- Index pour la table `dossard_tbl`
 --
 ALTER TABLE `dossard_tbl`
   ADD PRIMARY KEY (`ds_id`);
 
 --
--- Indexes for table `participant_tbl`
+-- Index pour la table `participant_tbl`
 --
 ALTER TABLE `participant_tbl`
   ADD PRIMARY KEY (`pt_id`),
@@ -263,7 +316,7 @@ ALTER TABLE `participant_tbl`
   ADD KEY `us_id` (`us_id`);
 
 --
--- Indexes for table `temps_tbl`
+-- Index pour la table `temps_tbl`
 --
 ALTER TABLE `temps_tbl`
   ADD PRIMARY KEY (`ts_id`),
@@ -271,84 +324,84 @@ ALTER TABLE `temps_tbl`
   ADD KEY `tr_id` (`tr_id`);
 
 --
--- Indexes for table `tour_tbl`
+-- Index pour la table `tour_tbl`
 --
 ALTER TABLE `tour_tbl`
   ADD PRIMARY KEY (`tr_id`),
   ADD KEY `crs_id` (`crs_id`);
 
 --
--- Indexes for table `user_tbl`
+-- Index pour la table `user_tbl`
 --
 ALTER TABLE `user_tbl`
   ADD PRIMARY KEY (`us_id`),
   ADD KEY `cl_id` (`cl_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `classeparticipante_tbl`
+-- AUTO_INCREMENT pour la table `classeparticipante_tbl`
 --
 ALTER TABLE `classeparticipante_tbl`
   MODIFY `clp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `classe_tbl`
+-- AUTO_INCREMENT pour la table `classe_tbl`
 --
 ALTER TABLE `classe_tbl`
   MODIFY `cl_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `course_tbl`
+-- AUTO_INCREMENT pour la table `course_tbl`
 --
 ALTER TABLE `course_tbl`
   MODIFY `crs_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `dossard_tbl`
+-- AUTO_INCREMENT pour la table `dossard_tbl`
 --
 ALTER TABLE `dossard_tbl`
   MODIFY `ds_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `participant_tbl`
+-- AUTO_INCREMENT pour la table `participant_tbl`
 --
 ALTER TABLE `participant_tbl`
-  MODIFY `pt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `pt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT for table `temps_tbl`
+-- AUTO_INCREMENT pour la table `temps_tbl`
 --
 ALTER TABLE `temps_tbl`
-  MODIFY `ts_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ts_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `tour_tbl`
+-- AUTO_INCREMENT pour la table `tour_tbl`
 --
 ALTER TABLE `tour_tbl`
   MODIFY `tr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `user_tbl`
+-- AUTO_INCREMENT pour la table `user_tbl`
 --
 ALTER TABLE `user_tbl`
-  MODIFY `us_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `us_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `classeparticipante_tbl`
+-- Contraintes pour la table `classeparticipante_tbl`
 --
 ALTER TABLE `classeparticipante_tbl`
   ADD CONSTRAINT `classeparticipante_tbl_ibfk_1` FOREIGN KEY (`cl_id`) REFERENCES `classe_tbl` (`cl_id`),
   ADD CONSTRAINT `classeparticipante_tbl_ibfk_2` FOREIGN KEY (`crs_id`) REFERENCES `course_tbl` (`crs_id`);
 
 --
--- Constraints for table `participant_tbl`
+-- Contraintes pour la table `participant_tbl`
 --
 ALTER TABLE `participant_tbl`
   ADD CONSTRAINT `participant_tbl_ibfk_1` FOREIGN KEY (`crs_id`) REFERENCES `course_tbl` (`crs_id`),
@@ -356,20 +409,20 @@ ALTER TABLE `participant_tbl`
   ADD CONSTRAINT `participant_tbl_ibfk_3` FOREIGN KEY (`us_id`) REFERENCES `user_tbl` (`us_id`);
 
 --
--- Constraints for table `temps_tbl`
+-- Contraintes pour la table `temps_tbl`
 --
 ALTER TABLE `temps_tbl`
   ADD CONSTRAINT `temps_tbl_ibfk_1` FOREIGN KEY (`tr_id`) REFERENCES `tour_tbl` (`tr_id`),
   ADD CONSTRAINT `temps_tbl_ibfk_2` FOREIGN KEY (`pt_id`) REFERENCES `participant_tbl` (`pt_id`);
 
 --
--- Constraints for table `tour_tbl`
+-- Contraintes pour la table `tour_tbl`
 --
 ALTER TABLE `tour_tbl`
   ADD CONSTRAINT `tour_tbl_ibfk_1` FOREIGN KEY (`crs_id`) REFERENCES `course_tbl` (`crs_id`);
 
 --
--- Constraints for table `user_tbl`
+-- Contraintes pour la table `user_tbl`
 --
 ALTER TABLE `user_tbl`
   ADD CONSTRAINT `user_tbl_ibfk_1` FOREIGN KEY (`cl_id`) REFERENCES `classe_tbl` (`cl_id`);
